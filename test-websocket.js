de71ba9ce7da50f4cad604023485b2c04a01a6f7
Syed Yaseen Jamal <yaseenjamal.70.syj@gmail.com>
2026-04-28 12:07:32 +0500

    Websocket test

---
+++ test-websocket.js
@@ -0,0 +1,25 @@
+const WebSocket = require('ws');
+
+// Connect to your WebSocket server
+const ws = new WebSocket('ws://localhost:9090/?callId=test-123');
+
+ws.on('open', () => {
+  console.log('Connected to WebSocket server');
+  
+  // Send some test audio data (16-bit PCM, 8kHz)
+  // This would normally be real audio from a microphone
+  const testAudio = Buffer.alloc(160); // 20ms of silence
+  
+  // Send audio frames
+  setInterval(() => {
+    ws.send(testAudio);
+  }, 20); // Send every 20ms (50fps)
+});
+
+ws.on('message', (data) => {
+  console.log('Received audio response:', data.length, 'bytes');
+});
+
+ws.on('error', (error) => {
+  console.error('WebSocket error:', error);
+});
