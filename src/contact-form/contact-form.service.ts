69dd89a7dd7e936dd631ea24fb435438ac6246b5
Syed Yaseen Jamal <yaseenjamal.70.syj@gmail.com>
2026-08-20 12:43:41 +0500

    Final updated

--- src/contact-form/contact-form.service.ts
+++ src/contact-form/contact-form.service.ts
@@ -34,8 +34,7 @@ ${dto.message}
       `.trim();
 
       await this.notificationService.sendEmail(
-        'Info@miaai.com.au, syeddyaseenn@gmail.com',
-        'jawadpasha256@gmail.com',
+        'Info@miaai.com.au, syeddyaseenn@gmail.com, jawadpasha256@gmail.com',
         'New Contact Us Form Submission',
         emailBody,
       );
